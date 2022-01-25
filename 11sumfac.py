d526dde160aadcfd27f94312756609dae7a5d504
evantjs <evtan@ucdavis.edu>
2022-01-25 01:07:45 -0800

    HW1 Done

---
+++ 11sumfac.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+# Write a program that computes the running sum from 1..n
+# Also, have it compute the factorial of n while you're at it
+# No, you may not use math.factorial()
+# Use the same loop for both calculations
+
+n = 5 
+sum = 0
+fac = 1
+for i in range ( 1 , n+1):
+	 sum += i 
+	 fac *= i
+
+print (sum, fac)
+
+
+n = 15
+sum = 0
+fac = 1
+for i in range (1 , n+1):
+	sum += i 
+	fac *= i 
+
+print (sum , fac)
+ 
+
+n = 120
+sum = 0
+fac = 1
+for i in range (1 , n+1):
+	sum += i 
+	fac *= i
+
+print (sum, fac)
+
+	
+# your code goes here
+
+"""
+python3 11sumfac.py
+5 15 120
+"""
