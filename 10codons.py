5060bf3e6589cb5b0dbe8b22dbc6346f9ec4d66b
evantjs <evtan@ucdavis.edu>
2022-01-24 22:43:03 -0800

    HW1

---
+++ 10codons.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Print out all the codons for the sequence below in reading frame 1
+# Use a 'for' loop
+
+dna = 'ATAGCGAATATCTCTCATGAGAGGGAA'
+k = 3
+for i in range(0, len(dna), k ):
+    print (dna[i:i+k])
+print ('End Of The Line')
+# your code goes here
+
+"""
+python3 10codons.py
+ATA
+GCG
+AAT
+ATC
+TCT
+CAT
+GAG
+AGG
+GAA
+"""
+
