848d9e5887b5be02d02571fc5ab47cc577db3049
evantjs <evtan@ucdavis.edu>
2022-02-02 18:07:03 -0800

    almost there

---
+++ 24frame.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# Write a program that prints out the position, frame, and letter of the DNA
+# Try coding this with a single loop
+# Try coding this with nested loops
+
+dna = 'ATGGCCTTT'
+
+for i in range(0, len(dna)):
+	
+	print(i, i % 3 , dna[i])
+
+
+for i in range(0, int(len(dna)/3)):
+	for j in range( 3 ):
+		print( 3*i+j, j, dna[3*i+j])
+		
+"""
+python3 24frame.py
+0 0 A
+1 1 T
+2 2 G
+3 0 G
+4 1 C
+5 2 C
+6 0 T
+7 1 T
+8 2 T
+"""
